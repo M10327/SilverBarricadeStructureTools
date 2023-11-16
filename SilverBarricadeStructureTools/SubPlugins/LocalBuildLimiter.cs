d3fbcf0725d66e102e4b843c2edc17cc15f1e680
Melody <99524525+M10327@users.noreply.github.com>
2023-11-15 22:08:09 -0600

    fixed local build limit

--- SilverBarricadeStructureTools/SubPlugins/LocalBuildLimiter.cs
+++ SilverBarricadeStructureTools/SubPlugins/LocalBuildLimiter.cs
@@ -15,29 +15,29 @@ namespace SilverBarricadeStructureTools.SubPlugins
     {
         public static void CheckStructures(ulong owner, Vector3 point, ref bool shouldAllow, ushort id)
         {
-            var cfg = SBST.Instance.cfg.LocalBuildLimiter.Structures;
-            foreach (var conf in cfg)
+            var cfg = SBST.Instance.cfg.LocalBuildLimiter.Barricades;
+            var c = cfg.Where(x => x.Ids.Contains(id)).FirstOrDefault();
+            if (c == null) return;
+
+            var checkrange = c.CheckRange * c.CheckRange;
+            List<RegionCoordinate> regions = new List<RegionCoordinate>();
+            Regions.getRegionsInRadius(point, checkrange, regions);
+
+            List<Transform> BarricadeTransforms = new List<Transform>();
+            StructureManager.getStructuresInRadius(point, checkrange, regions, BarricadeTransforms);
+
+            int count = 0;
+
+            foreach (var trans in BarricadeTransforms)
             {
-                if (!conf.Ids.Contains(id)) continue;
-                var checkrange = conf.CheckRange * conf.CheckRange;
-                List<RegionCoordinate> regions = new List<RegionCoordinate>();
-                Regions.getRegionsInRadius(point, checkrange, regions);
-
-                List<Transform> structureTransforms = new List<Transform>();
-                StructureManager.getStructuresInRadius(point, checkrange, regions, structureTransforms);
-
-                int count = 0;
-                foreach (var trans in structureTransforms)
+                var bar = StructureManager.FindStructureByRootTransform(trans);
+                if (bar == null) continue;
+                if (c.Ids.Contains(bar.asset.id)) count++;
+                if (count >= c.Limit)
                 {
-                    var bar = StructureManager.FindStructureByRootTransform(trans);
-                    if (bar == null) continue;
-                    if (conf.Ids.Contains(id)) count++;
-                    if (count >= conf.Limit)
-                    {
-                        UnturnedChat.Say((CSteamID)owner, SBST.Instance.Translate("LocalBuildLimit", conf.Limit, conf.Name, conf.CheckRange), SBST.Instance.MessageColor);
-                        shouldAllow = false;
-                        return;
-                    }
+                    UnturnedChat.Say((CSteamID)owner, SBST.Instance.Translate("LocalBuildLimit", c.Limit, c.Name, c.CheckRange), SBST.Instance.MessageColor);
+                    shouldAllow = false;
+                    return;
                 }
             }
         }
@@ -45,28 +45,28 @@ namespace SilverBarricadeStructureTools.SubPlugins
         public static void CheckBarricades(ulong owner, Vector3 point, ref bool shouldAllow, ushort id)
         {
             var cfg = SBST.Instance.cfg.LocalBuildLimiter.Barricades;
-            foreach (var conf in cfg)
+            var c = cfg.Where(x => x.Ids.Contains(id)).FirstOrDefault();
+            if (c == null) return;
+
+            var checkrange = c.CheckRange * c.CheckRange;
+            List<RegionCoordinate> regions = new List<RegionCoordinate>();
+            Regions.getRegionsInRadius(point, checkrange, regions);
+
+            List<Transform> BarricadeTransforms = new List<Transform>();
+            BarricadeManager.getBarricadesInRadius(point, checkrange, regions, BarricadeTransforms);
+
+            int count = 0;
+
+            foreach (var trans in BarricadeTransforms)
             {
-                if (!conf.Ids.Contains(id)) continue;
-                var checkrange = conf.CheckRange * conf.CheckRange;
-                List<RegionCoordinate> regions = new List<RegionCoordinate>();
-                Regions.getRegionsInRadius(point, checkrange, regions);
-
-                List<Transform> BarricadeTransforms = new List<Transform>();
-                BarricadeManager.getBarricadesInRadius(point, checkrange, regions, BarricadeTransforms);
-
-                int count = 0;
-                foreach(var trans in BarricadeTransforms)
+                var bar = BarricadeManager.FindBarricadeByRootTransform(trans);
+                if (bar == null) continue;
+                if (c.Ids.Contains(bar.asset.id)) count++;
+                if (count >= c.Limit)
                 {
-                    var bar = BarricadeManager.FindBarricadeByRootTransform(trans);
-                    if (bar == null) continue;
-                    if (conf.Ids.Contains(id)) count++;
-                    if (count >= conf.Limit)
-                    {
-                        UnturnedChat.Say((CSteamID)owner, SBST.Instance.Translate("LocalBuildLimit", conf.Limit, conf.Name, conf.CheckRange), SBST.Instance.MessageColor);
-                        shouldAllow = false;
-                        return;
-                    }
+                    UnturnedChat.Say((CSteamID)owner, SBST.Instance.Translate("LocalBuildLimit", c.Limit, c.Name, c.CheckRange), SBST.Instance.MessageColor);
+                    shouldAllow = false;
+                    return;
                 }
             }
         }
